972effed0f3a0f666bfc8c8c0b1cf3003ff61bdd
vtcipto <vtburakinan@gmail.com>
2026-05-29 00:53:00 +0300

    fix: tam ve hatasiz basecity-home meta verileri

--- app/layout.js
+++ app/layout.js
@@ -15,20 +15,11 @@ export const metadata = {
         action: {
           type: 'launch_frame',
           name: 'Basecity Home',
-          url: 'https://basecity-home.vercel.app/',
+          url: 'https://vercel.app',
         },
       },
     }),
+    // Base Geliştirici Doğrulama Kodu:
+    'base:app_id': '6a16a4c963a3a0a41fd577e9',
   },
 };
-
-
-
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
-    </html>
-  );
-}
